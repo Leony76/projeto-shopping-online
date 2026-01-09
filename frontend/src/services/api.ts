16e25cbdc3687f61b3ed8b7c28d9242c2576ca19
Leony Leandro Barros <leonyleandrobarros@gmail.com>
2026-01-09 18:35:37 -0300

    Código - 09/01/26 | 18:35

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,9 +3,6 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
-  headers: {
-    Accept: "application/json",
-  },
 });
 
 api.interceptors.request.use((config) => {
