5016ca385abf5057c473fedbb230d98598fa1d28
Leony Leandro Barros <leonyleandrobarros@gmail.com>
2026-01-09 14:30:22 -0300

    Código - 09/01/26 | 14:29

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -4,10 +4,17 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_origins' => [
-        'http://192.168.0.4:5173',
+        // 'http://192.168.0.4:5173',
+        'https://projeto-shopping-online-production-f4d2.up.railway.app',
     ],
 
-    'allowed_headers' => ['*'],
-    'allowed_methods' => ['*'],
+    'allowed_origins_patterns' => [],
 
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
 ];
