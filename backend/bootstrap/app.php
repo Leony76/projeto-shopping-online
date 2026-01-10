8e9e2246f614c0ca5629df0555788c65d3ec98ab
Leony Leandro Barros <leonyleandrobarros@gmail.com>
2026-01-10 10:35:46 -0300

    Código - 10/01/26 | 10:35

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
-        // $middleware->append(\Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,16 +1,12 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-
     'allowed_origins' => [
         'https://projeto-shopping-online-production-f4d2.up.railway.app',
     ],
-
     'allowed_headers' => ['*'],
-
     'supports_credentials' => false,
 ];
 
