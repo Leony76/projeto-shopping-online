74069e9973faec6437beb95b59b9c07209fe6a76
Leony Leandro Barros <leonyleandrobarros@gmail.com>
2026-01-12 08:43:35 -0300

    Cloudinary fix

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -37,7 +37,7 @@ class ProductController extends Controller
             'price' => 'required|numeric|min:1',
         ]);
 
-        $uploadedImage = Cloudinary::upload(
+        $uploadedImage = Cloudinary::uploadFile(
             $request->file('image')->getRealPath(),
             [
                 'folder' => 'products',
@@ -166,7 +166,7 @@ class ProductController extends Controller
         $product->update($validated);
 
         if ($request->hasFile('image')) {
-            $uploadedImage = Cloudinary::upload(
+            $uploadedImage = Cloudinary::uploadFile(
                 $request->file('image')->getRealPath(),
                 [
                     'folder' => 'products',
@@ -294,7 +294,7 @@ class ProductController extends Controller
             'image' => 'required|image|mimes:png,jpeg,jpg,webp',
         ]);
 
-        $uploadedImage = Cloudinary::upload(
+        $uploadedImage = Cloudinary::uploadFile(
             $request->file('image')->getRealPath(),
             [
                 'folder' => 'product_suggests',
--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -62,7 +62,6 @@ return [
 
         'cloudinary' => [
             'driver' => 'cloudinary',
-
             'cloud' => env('CLOUDINARY_CLOUD_NAME'),
             'key' => env('CLOUDINARY_API_KEY'),
             'secret' => env('CLOUDINARY_API_SECRET'),
